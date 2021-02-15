e5a7b33b52eb6d93c26591c37673feeceefa2eba
Dmitry <avhimkov@gmail.com>
2021-02-15 18:56:34 +0500

    added splite functions

--- main.go
+++ main.go
@@ -1,7 +1,40 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
 func main() {
-	QRgen("sdfdsf", "gen/qr.png")
+
+	str := "sdfdsf, sdpsd[pd, sdosidsoowoow"
+	result := strings.Split(str, ",")
+
+	for i := range result {
+		fmt.Println(result[i])
+		QRgen(result[i], "gen/qr.png")
+	}
+
+	// Part 1: open the file and scan it.
+	f, _ := os.Open("C:\\programs\\file.txt")
+	scanner := bufio.NewScanner(f)
+
+	// Part 2: call Scan in a for-loop.
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		// Split the line on commas.
+		parts := strings.Split(line, ",")
+
+		// Loop over the parts from the string.
+		for i := range parts {
+			fmt.Println(parts[i])
+		}
+		// Write a newline.
+		fmt.Println()
+	}
 
 	QRreader("gen/qr.png")
 }
