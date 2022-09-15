537d4c086ba11852d8ebefa20f4064e5075562a9
Dmitry <avhimkov@gmail.com>
2022-09-15 22:53:19 +0500

    added create files and refactoring

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"log"
 	"os"
 	"strings"
 )
@@ -10,7 +11,7 @@ import (
 func main() {
 
 	// Part 1: open the file and scan it.
-	f, _ := os.Open("file/file.txt")
+	f, _ := os.Open("files/readURL.txt")
 	scanner := bufio.NewScanner(f)
 
 	// Part 2: call Scan in a for-loop.
@@ -22,12 +23,13 @@ func main() {
 
 		// Loop over the parts from the string.
 		for i := range parts {
-			QRgen(parts[i], "gen/"+fmt.Sprint(i)+".png")
+			QRgen(parts[i], "genQR/"+fmt.Sprint(i+1)+".png")
 			// QRreader("gen/" + parts[i] + ".png")
-			fmt.Println(fmt.Sprint(i) + " " + "---" + parts[i])
+			fmt.Println(fmt.Sprint(i+1) + " " + "---" + " " + parts[i])
+			CreateTxt(fmt.Sprint(i+1) + ".txt")
 		}
-
 	}
+
 	/*
 		// Read QRcode image
 		qrfiles, err := ioutil.ReadDir("gen/")
@@ -42,4 +44,57 @@ func main() {
 	*/
 	// QRreader("gen/qr.png")
 
+}
+
+func CreateTxt(files string) os.File {
+	file, err := os.Create(files)
+
+	defer file.Close()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	/* 	fileName := files
+
+	   	val := "old\nfalcon\nsky\ncup\nforest\n"
+	   	data := []byte(val)
+
+	   	err := ioutil.WriteFile(fileName, data, 0644)
+
+	   	if err != nil {
+	   		log.Fatal(err)
+	   	}
+
+	   	fmt.Println("done")
+	*/
+
+	/*
+		fileName := "data.txt"
+
+		f, err := os.Create(fileName)
+
+		if err != nil {
+
+		    log.Fatal(err)
+		}
+
+		defer f.Close()
+
+		words := []string{"sky", "falcon", "rock", "hawk"}
+
+		for _, word := range words {
+
+		    _, err := f.WriteString(word + "\n")
+
+		    if err != nil {
+		        log.Fatal(err)
+		    }
+		}
+
+		fmt.Println("done")
+	*/
+
+	// fmt.Println("file created")
+	return *file
 }
